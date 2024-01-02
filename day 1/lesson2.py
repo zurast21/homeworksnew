0c09e927c4c93e03c356a7734a9f089e1df67ce8
zurast21 <1308606zurab@gmail.com>
2024-01-02 15:27:33 +0400

    second commit

---
+++ day 1/lesson2.py
@@ -0,0 +1,18 @@
+
+#name = "zura" #es aris STR (stringi)
+#age = 36 #es aris INT (integer) mteli ricxi
+#height = 190.1 #es aris float tipis cvladi (atciladi)
+#bolean (bool) tipis cvladi
+#knows_programing = True #True an False
+#is_ugly = False #snake case (weris stili)
+
+#surname = "chkadua"
+#print(name + " "  + surname)
+
+name = ("zurab")
+surname = ("chkadua")
+age = (36)
+height = (192)
+print(name +  " " +surname + " " + " " + str(age) + " " + str(height))
+
+
